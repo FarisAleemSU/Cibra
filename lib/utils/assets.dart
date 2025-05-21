de6da99ec2fa5d3666041b0917934792562bde87
FarisAleemSU <faris.ahmad1@sabanciuniv.edu>
2025-05-21 22:02:40 +0300

    Delete lib directory

--- lib/utils/assets.dart
+++
@@ -1,5 +0,0 @@
-class Assets {
-  static const String logo = 'assets/images/logo.png';
-  static const String breakfast = 'assets/images/breakfast.png';
-  static const String lunch = 'assets/images/lunch.png';
-}
